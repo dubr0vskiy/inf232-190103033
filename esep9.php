55f75fa95f915a28ac01c313ce6cbb6bf8ac2cd5
Damir Syzdykov <190103033@stu.sdu.edu.kz>
2021-02-09 23:45:51 +0600

    Created esep9.php
    Wrote the ninth problem

---
+++ esep9.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$my_str = 'Nur-Sultan is the capital of Republic of Kazakhstan';
+echo "The length of this text is ",strlen($my_str);
+?>
+
+</body>
+</html>
