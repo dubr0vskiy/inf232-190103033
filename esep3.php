3b97c1111460d7fb643eb839a2c88d8620dca5a1
Damir Syzdykov <190103033@stu.sdu.edu.kz>
2021-02-09 19:00:30 +0600

    Created esep3.php
    Wrote the third problem

---
+++ esep3.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$abc=10;
+$def=25;
+$first=pow($abc,2);
+$second=sqrt($def);
+
+echo "The square of ",$abc," equals to ",$first;
+echo "<br/>";
+echo "The square root of ",$def," is ",$second;
+
+?>
+</body>
+</html>
