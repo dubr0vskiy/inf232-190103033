5feaba3aee856a1b053bfa8e169bdf520ed16191
Damir Syzdykov <190103033@stu.sdu.edu.kz>
2021-02-09 18:47:43 +0600

    Created esep2.php
    Wrote the second problem

---
+++ esep2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$name="My name is Damir. ";
+$age="I'm 18. ";
+$uni=" Suleyman Demirel University.";
+	echo $name;echo $age;echo $uni;
+?>
+</body>
+</html>
